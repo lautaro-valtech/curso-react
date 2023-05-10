f9df5f788c5b2ad36e62e04f8fd29adab22caa76
lautaronahuelc <lautaro.coria@valtech.com>
2023-05-10 18:12:18 -0300

    update repository

---
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+
+    setCategories([newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory
+        onNewCategory={(newCategory) => onAddCategory(newCategory)}
+      />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
---
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputValue] = useState('One Punch');
+
+  const onInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (inputValue.trim().length <= 1) return;
+
+    onNewCategory(inputValue.trim());
+    setInputValue('');
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type='text'
+        placeholder='Buscar gifs'
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
+
+AddCategory.propTypes = {
+  onNewCategory: PropTypes.func.isRequired,
+};
