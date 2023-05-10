8df43b99070096b411b3a22589c43968989da262
lautaronahuelc <lautaro.coria@valtech.com>
2023-05-10 18:13:41 -0300

    update repository

--- 03-counter-app-vite/tests/CounterApp.test.jsx
+++ 03-counter-app-vite/tests/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { CounterApp } from '../src/CounterApp';
 
 describe('Pruebas en <CounterApp />', () => {
@@ -9,8 +9,37 @@ describe('Pruebas en <CounterApp />', () => {
     expect(container).toMatchSnapshot();
   });
 
-  test('Debe mostrar el valor inicial de 100 en (<CounterApp value={100} />', () => {
+  test('Debe mostrar el valor inicial de 100 en <CounterApp value={100} />', () => {
     render(<CounterApp value={initialValue} />);
+    expect(screen.getByRole('heading', { level: 2 }).innerHTML).toContain(
+      initialValue.toString()
+    );
+  });
+
+  test('Debe incrementar con el botón +1', () => {
+    render(<CounterApp value={initialValue} />);
+    fireEvent.click(screen.getByText('+1'));
+    expect(screen.getByRole('heading', { level: 2 }).innerHTML).toContain(
+      (initialValue + 1).toString()
+    );
+  });
+
+  test('Debe decrementar con el botón -1', () => {
+    render(<CounterApp value={initialValue} />);
+    fireEvent.click(screen.getByText('-1'));
+    expect(screen.getByRole('heading', { level: 2 }).innerHTML).toContain(
+      (initialValue - 1).toString()
+    );
+  });
+
+  test('Debe funcionar el botón de reset', () => {
+    render(<CounterApp value={initialValue} />);
+
+    fireEvent.click(screen.getByText('+1'));
+    fireEvent.click(screen.getByText('+1'));
+    fireEvent.click(screen.getByText('+1'));
+    fireEvent.click(screen.getByRole('button', { name: 'btn-reset' }));
+
     expect(screen.getByRole('heading', { level: 2 }).innerHTML).toContain(
       initialValue.toString()
     );
